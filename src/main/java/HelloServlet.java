45c93d75d86486fa33f63e2e9c5a8b6995beb75b
Christian <chrbudtz@gmail.com>
2021-09-22 13:43:56 +0200

    Initial commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "HelloServlet",
+        urlPatterns = "/api"
+)
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello!");
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getParameter("username"));
+        System.out.println(req.getParameter("password"));
+        PrintWriter writer = resp.getWriter();
+        resp.addCookie(new Cookie("ehr","Johnny"));
+
+        if ("pass".equals(req.getParameter("password"))){
+            resp.sendRedirect("welcome.html");
+            writer.println("Velkommen til!");
+        } else {
+            writer.println("Du må ikke komme ind!");
+        }
+    }
+}
